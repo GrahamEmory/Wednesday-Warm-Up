87baf5aff73f0d177444b69c84a41a0731588c81
GrahamEmory <gemory44@gmail.com>
2020-08-05 19:25:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#VARIABLE
+# something that can store a single piece of information
+name = "Spongebob"
+score = 0
+height = 62.5
+# Boolean data type
+# A piece of data that can be turned true or false
+# State variable 
+# describes what the current state of variable is  
+
+game_over = True
+
+# LOOPS - repeats code
+
+# For Loop - repeats code for a given amount of times
+
+for i in range(4):
+  print("Spongebob")
+
+# While loop - repeats code until its condition is met aka turns false 
+count = 0
+while count < 3:
+  print("listen before copying", count)
+  count += 1
+
+
